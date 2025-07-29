fa30f68a9a311461da3083d0ec1f5422ebbbac41
ZZW-vs <Zzw13675816057@outlook.com>
2025-07-29 17:05:09 +0800

    12th save / UI

--- 计算器.py
+++ 计算器.py
@@ -26,40 +26,38 @@ def update_result(result):
 # 创建主窗口
 root = tk.Tk()
 root.title("计算器")
-root.geometry("300x400")
-root.configure(bg='#F0F0F0')
+root.geometry("400x600")
+root.configure(bg='#202124')
 
 # 创建输入框
-entry = tk.Text(root, height=1, width=20, font=('Arial', 24), bg='#FFFFFF', relief=tk.SUNKEN, bd=2)
-entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10, sticky='we')
+entry = tk.Text(root, height=1, width=25, font=('Segoe UI', 24), bg='#202124', fg='#ffffff', relief=tk.FLAT, bd=0)
+entry.grid(row=0, column=0, columnspan=4, padx=20, pady=20, sticky='we')
 
 # 创建结果显示框
-result_entry = tk.Text(root, height=1, width=20, font=('Arial', 24), bg='#FFFFE0', relief=tk.SUNKEN, bd=2, state=tk.DISABLED)
-result_entry.grid(row=1, column=0, columnspan=4, padx=10, pady=10, sticky='we')
+result_entry = tk.Text(root, height=1, width=25, font=('Segoe UI', 24, 'bold'), bg='#202124', fg='#ffffff', relief=tk.FLAT, bd=0, state=tk.DISABLED)
+result_entry.grid(row=1, column=0, columnspan=4, padx=20, pady=20, sticky='we')
 
 # 使用 ttk.Style 创建自定义样式
 style = ttk.Style()
-style.configure('Rounded.TButton', relief=tk.FLAT, background="#EDEBEB", borderwidth=5, padding=5)
-style.map('Rounded.TButton', background=[('active', "#FFBB3C"), ('pressed', "#FFE344")])
+style.theme_use('clam')
+style.configure('Rounded.TButton', relief=tk.FLAT, background="#41424d", foreground="#ffffff", font=('Segoe UI', 18),
+                borderwidth=0, padding=10)
+style.map('Rounded.TButton', background=[('active', "#60616b")])
 
 # 按钮布局
 buttons = [
     ('7', 2, 0), ('8', 2, 1), ('9', 2, 2), ('/', 2, 3),
     ('4', 3, 0), ('5', 3, 1), ('6', 3, 2), ('*', 3, 3),
     ('1', 4, 0), ('2', 4, 1), ('3', 4, 2), ('-', 4, 3),
-    ('0', 5, 0), ('.', 5, 1), ('=', 5, 2), ('+', 5, 3)
+    ('0', 5, 0), ('.', 5, 1), ('=', 5, 2), ('+', 5, 3),
+    ('C', 6, 0), ('DEL', 6, 1)
 ]
 
 # 创建按钮
 for (text_button, row, col) in buttons:
-    command = calculate if text_button == '=' else lambda t=text_button: add_char(t)
-    bg_color = "#FFE344" if text_button == '=' else "#FFBB3C" if text_button in ['+', '-', '*', '/'] else "#EDEBEB"
-    btn = ttk.Button(root, text=text_button, width=10, style='Rounded.TButton', command=command)
-    btn.grid(row=row, column=col, padx=5, pady=5, sticky='nsew')
-
-# 清除按钮
-clear_btn = ttk.Button(root, text='C', width=40, style='Rounded.TButton', command=clear)
-clear_btn.grid(row=6, column=0, columnspan=4, sticky="we", padx=10, pady=5)
+    command = calculate if text_button == '=' else clear if text_button == 'C' else lambda t=text_button: add_char(t)
+    btn = ttk.Button(root, text=text_button, style='Rounded.TButton', command=command)
+    btn.grid(row=row, column=col, padx=15, pady=15, sticky='nsew')
 
 # 设置网格的权重
 for i in range(7):
